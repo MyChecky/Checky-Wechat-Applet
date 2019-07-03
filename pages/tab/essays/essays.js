297a2190998288c97a38a16ae664140806cbd7ae
LouisLuBrain <40519757+LouisLuBrain@users.noreply.github.com>
2019-07-03 15:34:50 +0800

    new switch

--- pages/tab/essays/essays.js
+++ pages/tab/essays/essays.js
@@ -1,4 +1,6 @@
 // pages/tab/essays/essays.js
+var util = require("../../../utils/util.js")
+
 Page({
 
   /**
@@ -11,8 +13,11 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    var time = util.formatTime(new Date());
+    this.setData({
+      time: time
+    });
   },
 
   /**
