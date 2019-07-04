ceb3605fc271623b0f415de14f1c74839f6f71f2
LouisLuBrain <40519757+LouisLuBrain@users.noreply.github.com>
2019-07-04 22:41:23 +0800

    utils

--- utils/util.js
+++ utils/util.js
@@ -13,8 +13,12 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
-// 重复日期转化
+// 重复日期转化S2B
 const formatRepeatDate = function(chooseDate){
+  if (chooseDate.length==0){
+    console.error("未选择重复日期")
+    return chooseDate
+  }
   var result = [0,0,0,0,0,0,0]
   for(var i = 0;i<chooseDate.length;i++){
     result[chooseDate[i]]=1
@@ -22,7 +26,52 @@ const formatRepeatDate = function(chooseDate){
   result = result.join('')
   return result
 }
+// 重复日期转化B2S
+const formatBiDate = function(biString){
+  var result = biString.split('')
+
+  if (result.length != 7) {
+    console.error("错误数据")
+    return biString
+  }
+
+  var date = []
+  for (var i = 0; i < 7; i++){
+    if(result[i]=="1"){
+      switch (i){
+        case 0:
+          date.push("日")
+          break
+        case 1:
+          date.push("一")
+          break
+        case 2:
+          date.push("二")
+          break
+        case 3:
+          date.push("三")
+          break
+        case 4:
+          date.push("四")
+          break
+        case 5:
+          date.push("五")
+          break
+        case 6:
+          date.push("六")
+          break
+      }
+    }
+    // result[i] = parseInt(result[i])
+  }
+  if(date.length==7){
+    return ["每天"]
+  }
+  date = date.join('  ')
+  return date
+}
 module.exports = {
   formatRepeatDate: formatRepeatDate,
+  formatBiDate: formatBiDate,
   formatTime: formatTime
 }
