9884e87515eda031412ec1574667c92dcab81fed
WWTobio <987761617@qq.com>
2019-07-18 00:35:43 +0800

    动态界面的完善

--- pages/tab/essays/essays.js
+++ pages/tab/essays/essays.js
@@ -2,28 +2,29 @@
 var util = require("../../../utils/util.js")
 Page({
   data: {
-    isLike: false,
-    isComment:false,
-
+    isComment: false,
+    likeSum:10,
     essays: [{
       userId: "用户123",
       essaysText: "背单词好快乐",
       essayTime: "7月2日 13:45",
-      likeSum:"20",
-      essayPic:"image/pic.jpg"
+      essayPic: "image/pic.jpg",
+      isLike:"false",
     },
     {
       userId: "用户231",
       essaysText: "背单词好痛苦,我今天花了10个小时背单词，头都大了，测试一下",
       essayTime: "7月3日 13:45",
-      likeSum:"12"
+      isLike: "true",
     }
     ],
   },
 
-  isLike: function () {
+  isLike: function (e) {
+    var index = e.currentTarget.dataset.index
     this.setData({
       isLike: !this.data.isLike,
+      likeSum: this.data.isLike ? this.data.likeSum - 1 : this.data.likeSum + 1
     })
   },
 
@@ -41,8 +42,8 @@ Page({
       url: './essaysDetail/essaysDetail',
     })
   },
-//创建打卡
-  essaysNew:function(){
+  //创建打卡
+  essaysNew: function () {
     wx.navigateTo({
       url: './essaysNew/essaysNew',
     })
@@ -50,10 +51,10 @@ Page({
 
 
   onLoad: function (options) {
-    
+
   },
 
-  isComment: function() {
+  isComment: function () {
     this.setData({
       isComment: !this.data.isComment,
     })
@@ -62,32 +63,30 @@ Page({
     })
   },
   //查看打卡详情
-  essaysClick: function() {
+  essaysClick: function () {
     wx.navigateTo({
       url: './essaysDetail/essaysDetail',
     })
   },
   //创建打卡
-  essaysNew: function() {
+  essaysNew: function () {
     wx.navigateTo({
       url: './essaysNew/essaysNew',
     })
   },
-  onLoad: function(options) {
 
-  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   }
 })
