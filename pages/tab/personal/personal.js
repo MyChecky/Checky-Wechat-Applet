778c64e5c4f5eef096171f8321ce094d50c6673b
lugf027 <161230578@qq.com>
2020-04-10 12:43:38 +0800

    个人页变化

--- pages/tab/personal/personal.js
+++ pages/tab/personal/personal.js
@@ -7,6 +7,9 @@ Page({
    */
   data: {
     userInfo: '',
+    ifAgree: true,
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     "itemList" : [
       {
         "subItemList": [
@@ -36,7 +39,36 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+      console.log("已有信息")
+      // this.sendInfo()
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 
+      // 所以此处加入 callback 以防止这种情况 
+      console.log("已授权，无信息")
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+      // this.sendInfo()
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理 
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -54,8 +86,82 @@ Page({
       userInfo: app.globalData.userInfo
     })
   },
+
+  login_in: function () {
+    console.log("准备发送的数据：")
+    console.log(app.globalData)
+    this.selectComponent("#toast").toastShow2("稍等", "fa-spinner fa-pulse")
+    // 向后台发送
+    wx.request({
+      url: app.getAbsolutePath() + '/wechat/login',
+      method: 'POST',
+      data: {
+        "code": app.globalData.code,
+        "userInfo": app.globalData.userInfo,
+        "location": app.globalData.location,
+        "baseIp": app.getAbsolutePath(),
+      },
+      success: (res) => {
+        console.log(res.data)
+        app.globalData.openId = res.data.states
+        app.globalData.sessionKey = res.data.sessionKey
+        app.globalData.userInfo.gender = res.data.userGender
+        app.globalData.userInfo.nickName = res.data.userNickname
+        app.globalData.userInfo.avatarUrl = res.data.userAvatar
+        console.log("globaldate", app.globalData)
+        // 这里是登陆后的一些页面/控件可见性信息
+        app.globalData.ifTrueMoneyAccess = res.data.ifTrueMoneyAccess
+        app.globalData.ifNewTaskHighSettingAccess = res.data.ifNewTaskHighSettingAccess
+        if (app.globalData.openId != "0") {
+          this.selectComponent("#toast").toastShow("登录成功", "fa-check", 1000)
+        }
+        else {
+          this.selectComponent("#toast").toastShow("登陆失败", "fa-remove", 1000)
+        }
+      },
+      fail: (err) => {
+        this.selectComponent("#toast").toastShow("登陆失败", "fa-remove", 1000)
+      }
+    })
+  },
+
+  getUserInfo: function (e) {
+
+    if (this.data.ifAgree) {
+      console.log("点击授权，可是不一定点击同意授权了", e.detail)
+      if(e.detail.errMsg === "getUserInfo:fail auth deny"){
+        this.selectComponent("#toast").toastShow("授权失败", "fa-remove", 1000)
+      } else if (e.detail.errMsg === "getUserInfo:ok"){
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+          userInfo: e.detail.userInfo,
+          hasUserInfo: true
+        })
+        this.login_in()
+      }
+    } else {
+      this.selectComponent("#toast").toastShow("未同意服务条款", "fa-remove", 1000)
+    }
+  },
+
+  openServiceContent: function () {
+    wx.navigateTo({
+      url: './serviceContent/serviceContent',
+    })
+  },
+
+  checkboxChange: function (e) {
+    this.data.ifAgree = !this.data.ifAgree
+    console.log(this.data.ifAgree)
+  },
+
   // 跳转
   jumpTo: function(e){
+    if (app.globalData.ifHasUserInfo === false) {
+      wx.navigateTo({
+        url: '../../index/index'
+      })
+    }
     var url = e.target.dataset.url
     var param = e.target.dataset.param
     console.log(url + '?' + param)
