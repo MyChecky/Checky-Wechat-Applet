49f45ddc05e77236190ac7037f360733198bbe0a
Minn <Minn--@outlook.com>
2020-07-06 03:38:14 +0800

    hot page

---
+++ pages/tab/hot/hot.js
@@ -0,0 +1,90 @@
+// pages/tab/hot/hot.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    imgs: [
+      'https://img.yzcdn.cn/vant/cat.jpeg',
+      'https://img.yzcdn.cn/vant/apple-2.jpg',
+      'https://img.yzcdn.cn/vant/apple-1.jpg'
+    ],
+    hotTopic: [
+      {
+        'index': 1,
+        'url': '#',
+        'param': '话题',
+        'name': '今天你上岸了吗',
+      },
+      {
+        'index': 2,
+        'url': '#',
+        'param': '话题',
+        'name': '托福分手',
+      },
+      {
+        'index': 3,
+        'url': '#',
+        'param': '话题',
+        'name': '雅思冲！',
+      },
+    ]
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
